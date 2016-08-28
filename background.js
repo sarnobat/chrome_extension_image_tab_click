d98df7e5aa959cf42584e6515833f6ef8af86835
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-28 10:30:48 -0700

    Minor changes (untested)

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+  //sendResponse({farewell:"goodbye"});
+  //console.debug(arg1);
+  chrome.tabs.create({ url: message.url, selected: false });
+});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	//alert();
 });
--- content.js
+++ content.js
@@ -3,7 +3,14 @@ document.body.addEventListener('click', function(arg1, arg2) {
 	var event = arg1;
 	if (event.altKey) {
 		if ('img' === arg1.path[0].tagName.toLowerCase()) {
-			window.open(arg1.path[0].src);
+			//window.open(arg1.path[0].src,'_blank');
+			// TODO: to open it in a background tab, do this
+			// in your background.js:
+			//chrome.tabs.create({ url: arg1.path[0].src });
+			
+			chrome.runtime.sendMessage({ url : arg1.path[0].src}, function(response) {
+			  //console.log(response.farewell);
+			});
 		}
 	}
 }, true);
