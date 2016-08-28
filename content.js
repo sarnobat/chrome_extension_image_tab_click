b43d34ebe2c5cd8fe5008674139436f7d093b960
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-27 19:45:25 -0700

    Minor changes (untested)

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
-document.body.addEventListener('click', function(arg1) {
-	alert('mouse was clicked: ' + arg1);
+document.body.addEventListener('click', function(arg1, arg2) {
+//	console.debug('mouse was clicked: ' + arg1);
+	console.debug(arg1.path[0]);
+	window.open(arg1.path[0].src);
 }, true);
